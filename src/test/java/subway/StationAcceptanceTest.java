1c9c27e81fbec94fab79896aea77bb3cbf4aceca
polymorph <33543196+dahyeong-yun@users.noreply.github.com>
2024-07-07 16:00:19 +0900

    🚀 1단계 - 지하철역 인수 테스트 작성 (#1188)
    * test: 구글 페이지 접근 테스트 추가
    
    * test: 지하철역 인수 테스트 추가

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -27,41 +27,86 @@ public class StationAcceptanceTest {
     @Test
     void createStation() {
         // when
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "강남역");
-
-        ExtractableResponse<Response> response =
-                RestAssured.given().log().all()
-                        .body(params)
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .when().post("/stations")
-                        .then().log().all()
-                        .extract();
+        ExtractableResponse<Response> response = createStation("강남역");
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 
         // then
-        List<String> stationNames =
-                RestAssured.given().log().all()
-                        .when().get("/stations")
-                        .then().log().all()
-                        .extract().jsonPath().getList("name", String.class);
+        List<String> stationNames = findAllStations();
+
         assertThat(stationNames).containsAnyOf("강남역");
     }
+
 
     /**
      * Given 2개의 지하철역을 생성하고
      * When 지하철역 목록을 조회하면
      * Then 2개의 지하철역을 응답 받는다
      */
-    // TODO: 지하철역 목록 조회 인수 테스트 메서드 생성
+    @DisplayName("지하철역 목록을 조회한다.")
+    @Test
+    void retrieveStation() {
+        // given
+        createStation("강남역");
+        createStation("을지로4가역");
+
+        // when
+        List<String> stationNames = findAllStations();
+
+        // then
+        assertThat(stationNames)
+                .contains("강남역", "을지로4가역");
+        assertThat(stationNames.size()).isEqualTo(2);
+    }
+
 
     /**
      * Given 지하철역을 생성하고
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    // TODO: 지하철역 제거 인수 테스트 메서드 생성
+    @DisplayName("지하철역을 삭제한다.")
+    @Test
+    void deleteStation() {
+        // given
+        createStation("강남역");
+        ExtractableResponse<Response> response = createStation("을지로4가역");
 
+        String createdStationId = response.body().jsonPath().getString("id");
+
+        // when
+        deleteStation(createdStationId);
+        List<String> stationNames = findAllStations();
+
+        // then
+        assertThat(stationNames).contains("강남역");
+        assertThat(stationNames).doesNotContain("을지로4가역");
+        assertThat(stationNames.size()).isEqualTo(1);
+    }
+
+    private ExtractableResponse<Response> createStation(String stationName) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", stationName);
+
+        return RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations")
+                .then().log().all()
+                .extract();
+    }
+
+    private List<String> findAllStations() {
+        return RestAssured.given().log().all()
+                .when().get("/stations")
+                .then().log().all()
+                .extract().jsonPath().getList("name", String.class);
+    }
+
+    private void deleteStation(String stationId) {
+        RestAssured.given().log().all()
+                .when().delete("/stations/" + stationId)
+                .then().log().all();
+    }
 }
